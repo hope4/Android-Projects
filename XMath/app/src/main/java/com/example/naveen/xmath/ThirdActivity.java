7937365a37bce9df7da1cb56d428d1672c75c35d
Navin <naveen.kumar131196@gmail.com>
2018-08-03 16:22:03 +0530

    X Math appfinal version, status working

--- XMath/app/src/main/java/com/example/naveen/xmath/ThirdActivity.java
+++ XMath/app/src/main/java/com/example/naveen/xmath/ThirdActivity.java
@@ -2,6 +2,7 @@ package com.example.naveen.xmath;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -15,9 +16,12 @@ import java.sql.BatchUpdateException;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
+import java.util.Locale;
 
 public class ThirdActivity extends AppCompatActivity {
 
+
+   private static final long COUNTDOWN_IN_MILLIS = 11000;
 
    private  QuestionLibrary mQuestionLibrary = new QuestionLibrary();
    private TextView textViewscore;
@@ -39,6 +43,9 @@ public class ThirdActivity extends AppCompatActivity {
    private Button bdot;
    private Button bclr;
 
+
+   private CountDownTimer countDownTimer;
+   private long timeleftInMillis;
 
    private String answer;
    private int score = 0;
@@ -72,6 +79,8 @@ public class ThirdActivity extends AppCompatActivity {
 
         editText.setText("");
         updatequestion();
+        timeleftInMillis = COUNTDOWN_IN_MILLIS;
+        startcountDown();
 
         b0.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -158,56 +167,121 @@ public class ThirdActivity extends AppCompatActivity {
         buttonenter.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-               // Toast.makeText(getApplicationContext(),answer,Toast.LENGTH_SHORT).show();
-               // Toast.makeText(getApplicationContext(),editText.getText(),Toast.LENGTH_SHORT).show();
+                // Toast.makeText(getApplicationContext(),answer,Toast.LENGTH_SHORT).show();
+                // Toast.makeText(getApplicationContext(),editText.getText(),Toast.LENGTH_SHORT).show();
                 int n = mQuestionLibrary.getQuestioncount();
-              //  String m = String.valueOf(questionnumber);
-               // Toast.makeText(getApplicationContext(),m,Toast.LENGTH_SHORT).show();
-                if(questionnumber < n) {
-                    if (editText.getText().toString().equals(answer)) {
-                        score = score + 10;
-                        updateScore(score);
-                        editText.setText("");
-                        updatequestion();
+                //  String m = String.valueOf(questionnumber);
+                // Toast.makeText(getApplicationContext(),m,Toast.LENGTH_SHORT).show();
+                    if (questionnumber < n) {
+                        //Toast.makeText(getApplicationContext(),answer,Toast.LENGTH_SHORT).show();
+                        if (editText.getText().toString().equals(answer)) {
+                            score = score + 10;
+                            updateScore(score);
+                            editText.setText("");
+                            updatequestion();
+                            countDownTimer.cancel();
+                            timeleftInMillis = COUNTDOWN_IN_MILLIS;
+                            startcountDown();
 
-                        Toast.makeText(getApplicationContext(), "Correct", Toast.LENGTH_SHORT).show();
+                            Toast.makeText(getApplicationContext(), "Correct", Toast.LENGTH_SHORT).show();
 
-                    } else if(editText.getText().toString().matches("")){
-                        Toast.makeText(getApplicationContext(), "Enter answer first", Toast.LENGTH_SHORT).show();
-                    }
-                    else {
-                        Toast.makeText(getApplicationContext(), "wrong", Toast.LENGTH_SHORT).show();
-                        editText.setText("");
-                        updatequestion();
-                    }
-                }
-                else{
-                   // Intent gotoFirst = new Intent(ThirdActivity.this,FirstActivity.class);
-                   // startActivity(gotoFirst);
-                    if (editText.getText().toString().equals(answer)) {
-                        score = score + 10;
-                        updateScore(score);
-                        editText.setText("");
-                        textViewquestion.setText("");
+                        } else if (editText.getText().toString().matches("")) {
+                            Toast.makeText(getApplicationContext(), "Enter answer first", Toast.LENGTH_SHORT).show();
+                        } else {
+                            score = score - 5;
+                            updateScore(score);
+                            editText.setText("");
+                            if(score <= 0){
+                                Toast.makeText(getApplicationContext(),"You are here",Toast.LENGTH_SHORT).show();
+                                textViewquestion.setText("You lost, X_Math Over!! :( \n Try again!!\nYour score is: "+score);
 
-                        Toast.makeText(getApplicationContext(), "Correct", Toast.LENGTH_SHORT).show();
+                                textViewscore.setText("");
+                                buttonenter.setEnabled(false);
+                                b0.setEnabled(false);
+                                b1.setEnabled(false);
+                                b2.setEnabled(false);
+                                b3.setEnabled(false);
+                                b4.setEnabled(false);
+                                b5.setEnabled(false);
+                                b6.setEnabled(false);
+                                b7.setEnabled(false);
+                                b8.setEnabled(false);
+                                b9.setEnabled(false);
+                                bdot.setEnabled(false);
+                                bclr.setEnabled(false);
+                                timeleftInMillis=0;
+                                updateCountDownText();
+                                countDownTimer.cancel();
 
-                    } else if(editText.getText().toString().matches("")){
-                        Toast.makeText(getApplicationContext(), "Enter answer first", Toast.LENGTH_SHORT).show();
+
+                            }
+                            else{
+                                updatequestion();
+                                countDownTimer.cancel();
+                                timeleftInMillis = COUNTDOWN_IN_MILLIS;
+                                startcountDown();
+                            }
+
+                        }
+
+                    } else {
+                        // Intent gotoFirst = new Intent(ThirdActivity.this,FirstActivity.class);
+                        // startActivity(gotoFirst);
+                        if (editText.getText().toString().equals(answer)) {
+                            score = score + 10;
+                            updateScore(score);
+                            editText.setText("");
+                            textViewquestion.setText("");
+
+                            Toast.makeText(getApplicationContext(), "Correct", Toast.LENGTH_SHORT).show();
+
+                        } else if (editText.getText().toString().matches("")) {
+                            Toast.makeText(getApplicationContext(), "Enter answer first", Toast.LENGTH_SHORT).show();
+                        } else {
+                            score = score - 5;
+                            updateScore(score);
+                            if(score <= 0){
+                              //  Toast.makeText(getApplicationContext(),"You are here",Toast.LENGTH_SHORT).show();
+                                textViewquestion.setText("You lost, X_Math Over!! :( \n Try again!!\nYour score is: "+score);
+                                editText.setFocusable(false);
+                                textViewscore.setText("");
+                                buttonenter.setEnabled(false);
+                                b0.setEnabled(false);
+                                b1.setEnabled(false);
+                                b2.setEnabled(false);
+                                b3.setEnabled(false);
+                                b4.setEnabled(false);
+                                b5.setEnabled(false);
+                                b6.setEnabled(false);
+                                b7.setEnabled(false);
+                                b8.setEnabled(false);
+                                b9.setEnabled(false);
+                                bdot.setEnabled(false);
+                                bclr.setEnabled(false);
+                                timeleftInMillis=0;
+                                updateCountDownText();
+                                countDownTimer.cancel();
+
+                            }
+                            editText.setText("");
+                            textViewquestion.setText("");
+                        }
+                        Toast.makeText(getApplicationContext(), "End of questions", Toast.LENGTH_SHORT).show();
+                        textViewquestion.setText("Congratulations!! :)\nYou have finished X_Math with\nScore: "+score);
+                        textViewscore.setText("");
+                        editText.setEnabled(false);
+
                     }
-                    else {
-                        Toast.makeText(getApplicationContext(), "wrong", Toast.LENGTH_SHORT).show();
-                        editText.setText("");
-                        textViewquestion.setText("");
-                    }
-                    Toast.makeText(getApplicationContext(),"End of questions",Toast.LENGTH_SHORT).show();
-                }
+
+
+
+
             }
         });
         Buttonend.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(getApplicationContext(),"U have ended the game\nYour score is:"+score,Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(),"U have ended X_Math\nYour score is: "+score,Toast.LENGTH_SHORT).show();
                 Intent gotoFirst = new Intent(ThirdActivity.this,FirstActivity.class);
                 startActivity(gotoFirst);
             }
@@ -216,7 +290,60 @@ public class ThirdActivity extends AppCompatActivity {
 
     }
 
+    private void startcountDown(){
+       countDownTimer = new CountDownTimer(timeleftInMillis,1000) {
+           @Override
+           public void onTick(long l) {
+               timeleftInMillis = l;
+               updateCountDownText();
+           }
+
+           @Override
+           public void onFinish() {
+            if(editText.getText().toString().matches("")){
+                score = score - 5;
+                updateScore(score);
+            }
+            if(questionnumber < mQuestionLibrary.getQuestioncount() && score > 0) {
+                updatequestion();
+                countDownTimer.cancel();
+                timeleftInMillis = COUNTDOWN_IN_MILLIS;
+                startcountDown();
+            }
+            else{
+                timeleftInMillis = 0;
+                updateCountDownText();
+                textViewquestion.setText("Congratulations!! :)\nYou have finished X_Math with\nScore: "+score);
+                editText.setFocusable(false);
+                textViewscore.setText("");
+                buttonenter.setEnabled(false);
+                b0.setEnabled(false);
+                b1.setEnabled(false);
+                b2.setEnabled(false);
+                b3.setEnabled(false);
+                b4.setEnabled(false);
+                b5.setEnabled(false);
+                b6.setEnabled(false);
+                b7.setEnabled(false);
+                b8.setEnabled(false);
+                b9.setEnabled(false);
+                bdot.setEnabled(false);
+                bclr.setEnabled(false);
+                countDownTimer.cancel();
+            }
+            }
+       }.start();
+    }
+
+    private void updateCountDownText(){
+        int minutes = (int) (timeleftInMillis / 1000 ) /60;
+        int seconds = (int) (timeleftInMillis/1000) % 60;
+        String timeformatted = String.format(Locale.getDefault(),"%02d:%02d",minutes,seconds);
+        textViewdisplaytimer.setText(timeformatted);
+    }
+
     public void updatequestion(){
+
         int questionnum = questionnumber + 1;
         textViewquestion.setText("Question : "+ questionnum + "\n\n" + mQuestionLibrary.getQuestion(questionnumber));
         answer = mQuestionLibrary.getCorrectAnswer(questionnumber);
@@ -224,7 +351,9 @@ public class ThirdActivity extends AppCompatActivity {
     }
 
     public void updateScore(int point){
-       textViewscore.setText("Score :"+ point);
+       textViewscore.setText("Score : "+ point);
     }
+
+
 
 }
